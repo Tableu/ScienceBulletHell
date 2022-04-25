d7ccb751de5432d815ebf62d16cd0b38e1d7f1e3
nat <87210044+fokai7@users.noreply.github.com>
2022-04-24 17:34:30 -0700

    title music implementation

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -3,6 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
+    public AudioSource source;
+    public AudioClip audioClip;
+    private void Start()
+    {
+        source = GetComponent<AudioSource>();
+        source.loop = true;
+        source.PlayOneShot(audioClip);
+    }
     public void OnClick()
     {
         SceneManager.LoadScene("Scenes/GameScene");
